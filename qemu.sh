85453ba9b13abd27d02d884d1f2a7e44c53cb4da
filippo <filippo.tortomasi@gmail.com>
2018-11-14 22:41:38 +0100

    Update qemu.sh

--- qemu.sh
+++ qemu.sh
@@ -1,4 +1,5 @@
-#! /bin/bash
+#!/bin/bash
+
 #Install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -22,5 +23,3 @@ wget https://github.com/dhruvvyas90/qemu-rpi-kernel/raw/master/versatile-pb.dtb
 
 #Run 
 qemu-system-arm -kernel kernel-qemu-4.9.59-stretch -cpu arm1176 -m 256 -M versatilepb -dtb versatile-pb.dtb -no-reboot -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" -net nic -net user,hostfwd=tcp::5022-:22 -drive "file=2018-10-09-raspbian-stretch-lite.img,index=0,media=disk,format=raw"
-
-
